b8265510448d00a53790724aa0e8db231a7da17b
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2021-04-19 17:04:31 -0500

    feat: restaurando configuracion antes de passport

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('contactos', 'ContactoController')->middleware('auth:api');
+Route::resource('contactos', 'ContactoController');
